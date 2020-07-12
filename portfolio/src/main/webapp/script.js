2eb96b9448a667489b77b20472fef7c06f166d56
Chelsea <cchoi9808@gmail.com>
2020-07-12 11:09:30 +0800

    Complete Week 1 and Step 1

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,14 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomQuote() {
+  const quotes =
+      ['Do or do not there is no try', 'I know', 'My the Force be with you', 'Never tell me the odds'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const quoteContainer = document.getElementById('greeting-container');
+  quoteContainer.innerText = quote;
 }
